05b6f1e9b70f90bc843d154f4d8a0d10a0fa1974
sohag <ihsohag84@gmail.com>
2023-09-12 19:00:31 +0600

    form validation

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:developer';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chat_app/api/apis.dart';
 import 'package:chat_app/helper/dialogs.dart';
@@ -18,9 +20,12 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      /// for hiding keyboard
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
         appBar: AppBar(
@@ -55,84 +60,115 @@ class _ProfileScreenState extends State<ProfileScreen> {
             label: Text("Logout"),
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              SizedBox(),
-              Center(
-                child: Stack(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(mq.height * 0.1),
-                      child: CachedNetworkImage(
-                        width: mq.height * 0.2,
-                        height: mq.height * 0.2,
-                        fit: BoxFit.fill,
-                        imageUrl: widget.chatUser.image,
-                        errorWidget: (context, url, error) => Icon(Icons.error),
-                      ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  SizedBox(),
+                  Center(
+                    child: Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(mq.height * 0.1),
+                          child: CachedNetworkImage(
+                            width: mq.height * 0.2,
+                            height: mq.height * 0.2,
+                            fit: BoxFit.fill,
+                            imageUrl: widget.chatUser.image,
+                            errorWidget: (context, url, error) =>
+                                Icon(Icons.error),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: MaterialButton(
+                            onPressed: () {},
+                            elevation: 1,
+                            color: Colors.white,
+                            shape: CircleBorder(),
+                            child: Icon(Icons.edit, color: Colors.blue),
+                          ),
+                        ),
+                      ],
                     ),
-                    Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: MaterialButton(
-                        onPressed: () {},
-                        elevation: 1,
-                        color: Colors.white,
-                        shape: CircleBorder(),
-                        child: Icon(Icons.edit, color: Colors.blue),
-                      ),
+                  ),
+                  SizedBox(height: mq.height * 0.02),
+                  Text(
+                    widget.chatUser.email,
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 16,
                     ),
-                  ],
-                ),
-              ),
-              SizedBox(height: mq.height * 0.02),
-              Text(
-                widget.chatUser.email,
-                style: TextStyle(
-                  color: Colors.black54,
-                  fontSize: 16,
-                ),
-              ),
-              SizedBox(height: mq.height * 0.03),
-              TextFormField(
-                initialValue: widget.chatUser.name,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
                   ),
-                  prefixIcon: Icon(Icons.person),
-                  hintText: "eg. Abdullah Al Raiyan",
-                  label: Text("Name"),
-                ),
-              ),
-              SizedBox(height: mq.height * 0.03),
-              TextFormField(
-                initialValue: widget.chatUser.about,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
+                  SizedBox(height: mq.height * 0.03),
+                  TextFormField(
+                    initialValue: widget.chatUser.name,
+                    onSaved: (val) {
+                      APIs.me.name = val ?? "";
+                    },
+                    validator: (val) {
+                      if (val != null && val.isNotEmpty) {
+                        return null;
+                      } else {
+                        return "Required Field";
+                      }
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      prefixIcon: Icon(Icons.person),
+                      hintText: "eg. Abdullah Al Raiyan",
+                      label: Text("Name"),
+                    ),
                   ),
-                  prefixIcon: Icon(Icons.info_outline),
-                  hintText: "eg. Feeling Happy",
-                  label: Text("About"),
-                ),
+                  SizedBox(height: mq.height * 0.03),
+                  TextFormField(
+                    initialValue: widget.chatUser.about,
+                    onSaved: (val) {
+                      APIs.me.about = val ?? "";
+                    },
+                    validator: (val) {
+                      if (val != null && val.isNotEmpty) {
+                        return null;
+                      } else {
+                        return "Required Field";
+                      }
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      prefixIcon: Icon(Icons.info_outline),
+                      hintText: "eg. Feeling Happy",
+                      label: Text("About"),
+                    ),
+                  ),
+                  SizedBox(height: mq.height * 0.05),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        log("inside validator");
+                      }
+                    },
+                    label: Text(
+                      "Update",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    icon: Icon(Icons.edit, size: 28),
+                    style: ElevatedButton.styleFrom(
+                      shape: StadiumBorder(),
+                      minimumSize: Size(mq.width * 0.5, mq.height * 0.055),
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(height: mq.height * 0.05),
-              ElevatedButton.icon(
-                onPressed: () {},
-                label: Text(
-                  "Update",
-                  style: TextStyle(fontSize: 16),
-                ),
-                icon: Icon(Icons.edit, size: 28),
-                style: ElevatedButton.styleFrom(
-                  shape: StadiumBorder(),
-                  minimumSize: Size(mq.width * 0.5, mq.height * 0.055),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
